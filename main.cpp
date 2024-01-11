f8fd43865b56a88af1bb648ee044dc7842cd5f09
CertifiedMathesonMoment <339471856@gotvdsb.ca>
2024-01-11 13:23:37 -0500

    testing shane images

--- main.cpp
+++ main.cpp
@@ -3,41 +3,56 @@
 
 using namespace std;
 
-int main () {
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
 
-    cout << "Hello World" << endl;
+
+
+
+
+
+
+int main()
+{   
+    int shopBoxWidth = 125;
+    int shopBoxHeight = 320;
+    int dispBoxWidth = 925;
+    int dispBoxHeight = 128;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
     InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    
+
+    Texture2D texture = LoadTexture("X:/My Drive/Smithmas 2/Unit Project/CookieClicker/AssetLibrary/Shane1.png");
+
     SetTargetFPS(60);
 
-    while (WindowShouldClose() == false){
+    while (WindowShouldClose() == false)
+    {
+        
+
+
+        // Draw
         BeginDrawing();
-        ClearBackground(BLACK);
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
-
-        if(ball_x + ball_radius >= screenWidth  || ball_x - ball_radius <= 0)
+        ClearBackground(RAYWHITE);
+        DrawRectangle(590, 161, dispBoxWidth, dispBoxHeight, RED);
+        int shaneVar = 5;
+        for (int i = 0; i < 20; i++)
         {
-            ball_speed_x *= -1;
+            shaneVar *=-1;
+            DrawTexture(texture, 590 + i*40, 175 + shaneVar, WHITE);
         }
+        // Draw the texture at the center of the screen
+       
 
-        if(ball_y + ball_radius >= screenHeight  || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
 
-        DrawCircle(ball_x,ball_y,ball_radius, WHITE);
+
+        
+
         EndDrawing();
     }
 
     CloseWindow();
+    UnloadTexture(texture);
     return 0;
 }
