780b4cb0470828af6c2670ed8b7267111dfe31f9
MilindMaheshwari <95548758+MilindMaheshwari@users.noreply.github.com>
2024-01-23 13:55:54 -0500

    streamlined Achievement class

--- AchievementClass.hpp
+++ AchievementClass.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <raylib.h>
+#include <functional>
 
 #pragma once
 
@@ -16,6 +17,10 @@ class Achievement{
         string achievementName;
         string achievementMessage;
         
+
+        function<bool()> condition;  
+        function<void()> whatToDo;
+        
         Rectangle achievementBox = {590, 780, 910, 240};
 
 
@@ -23,39 +28,60 @@ class Achievement{
 
     public:
 
-        Achievement(string achievementName, string achievementMessage){
+        Achievement(string achievementName, string achievementMessage, function<void()> whatToDo){
 
             this->achievementName = achievementName;
             this->achievementMessage = achievementMessage;
+            this->whatToDo = whatToDo;
 
         }
 
 
-        bool shouldBeOnScreen(){
+        // bool shouldBeOnScreen(){
 
+        //     if(achieved && (GetTime() - startTime) <= 4){       //If the achievement has been achieved less than four seconds ago, 
+            
+        //         DrawRectangleRec(achievementBox, RED);
+        //         DrawText(TextFormat("%s: %s", achievementName.c_str(), achievementMessage.c_str()), achievementBox.x, achievementBox.y, 12, GREEN);
+
+        //         return true;
+        //     }
+        //     else{                   //If the achievement hasn't been reached yet OR its been more than 4 seconds, 
+        //         return false;
+        //     }
+
+        // }
+
+        void checkIfAchieved(bool conditionReached){
+
+            if(!achieved && conditionReached){
+
+                achieved = true;
+                startTime = GetTime();
+                whatToDo(); //Run the command passed
+
+            }
+            
             if(achieved && (GetTime() - startTime) <= 4){       //If the achievement has been achieved less than four seconds ago, 
             
                 DrawRectangleRec(achievementBox, RED);
-                DrawText(TextFormat("%s: %s", achievementName.c_str(), achievementMessage.c_str()), achievementBox.x, achievementBox.y, 12, GREEN);
-
-                return true;
+                DrawText(TextFormat("%s: %s", achievementName.c_str(), achievementMessage.c_str()), achievementBox.x, achievementBox.y, 30, GREEN);
             }
-            else{                   //If the achievement hasn't been reached yet OR its been more than 4 seconds, 
-                return false;
-            }
-
         }
 
-        void setAchieved(){
+        // void setAchieved(){
             
-            if(achieved != true){   //If achievement isn't already achieved
-                achieved = true;    
-                startTime = GetTime();  //Store the current time in the startTime variable
-            }
-        }
+        //     if(!achieved){   //If achievement isn't already achieved
+        //         achieved = true;    
+        //         startTime = GetTime();  //Store the current time in the startTime variable
 
-        bool getAchieved(){
-            return achieved;
-        }
+        //         whatToDo();
+
+        //     }
+        // }
+
+        // bool getAchieved(){
+        //     return achieved;
+        // }
 
 };
